ecd094261b9faaf1b81b47272011ca4de71576b9
Stephen <rstephenirving@gmail.com>
2017-09-29 12:44:34 -0400

    Comments functionality added. Hopefully won't crash the entire site :)

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
   url(r'^news$', views.get_news), # POST request(s) to news API
   url(r'^logout$', views.logout),
   url(r'^weather$', views.weather), # POST request(s) to weather API
-  url(r'^notes$', views.notes), # user retrieves notes from DB
   url(r'^reading_list/(?P<username>)$', views.reading_list), # publicly available reading list. user can set to private in options
   url(r'^add_story$', views.add_story), # POST to add story to reading leist
   url(r'^delete_story$', views.delete_story), # POST request for user to delete stories  from reading list
+  url(r'^notes$', views.notes), # user retrieves notes from DB
   url(r'^new_note$', views.new_note), # POST new note to database
   url(r'^delete_note$', views.delete_note) # POST that deletes a user's note
 ]
